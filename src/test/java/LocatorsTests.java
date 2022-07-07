01fc5ee66f043941e1a1504e4a86a37c412d1aa8
PatternsG <kovalevskiiivan@gmail.com>
2022-07-07 20:03:48 +0300

    Lesson 8 (locators) AfterClass

--- src/test/java/LocatorsTests.java
+++ src/test/java/LocatorsTests.java
@@ -4,10 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +25,7 @@ public class LocatorsTests {
         driver.get("https://www.saucedemo.com/");
     }
 
-    @AfterMethod
+    @AfterClass
     public void quit(){
         driver.quit();
     }
